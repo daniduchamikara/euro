0a8e6ecc90f9beb92eea36e94554c76b443844d0
dchamikara <dchamikara@iom.int>
2024-08-16 11:44:48 +0530

    correction

--- process_form.php
+++ process_form.php
@@ -23,12 +23,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     try {
         // Server settings
         $mail->isSMTP();                                           // Send using SMTP
-        $mail->Host       = 'smtp.gmail.com';                      // Set the SMTP server to send through
+        $mail->Host       = 'smtp.host.tld';                      // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                  // Enable SMTP authentication
-        $mail->Username   = 'daniduchamikara@gmail.com';           // SMTP username
-        $mail->Password   = 'qpie zjtu cdln ntak'; // SMTP password (use an app password if you have 2FA enabled)
+        $mail->Username   = 'contact@eurolankaweb.com';           // SMTP username
+        $mail->Password   = '(uaiH;NZ?zd~'; // SMTP password (use an app password if you have 2FA enabled)
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;        // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-        $mail->Port       = 587;                                   // TCP port to connect to
+        $mail->Port       = 465;                                   // TCP port to connect to
 
         // Recipients for admin notification
         $mail->setFrom($email, $name);                              // Sender's email and name
